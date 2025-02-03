aea354e40baa3ed23a547cf87bd33687afb47d77
Yazan <100063679@ku.ac.ae>
2025-02-03 10:00:29 +0400

    Primitive types 3

--- Lab2/Primitive_types/primitive_types3_sol.rs
+++ Lab2/Primitive_types/primitive_types3_sol.rs
@@ -1,6 +1,6 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    let a = [42; 100]
+    let a = [42; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
